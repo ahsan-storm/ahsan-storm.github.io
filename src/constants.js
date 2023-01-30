182bad3b3ad9e1b3383d15532772c53e5fb9ca10
muhammadahsan <mahsan@invidi.com>
2023-01-30 13:03:55 +0100

    adds `2023` vacations

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,22 @@
 export const data = [
    {
+      "year": "2023",
+      "vacations": {
+         "January": 19,
+         "February": 0,
+         "March": 0,
+         "April": 0,
+         "May": 0,
+         "June": 0,
+         "July": 0,
+         "August": 0,
+         "September": 0,
+         "October": 0,
+         "November": 0,
+         "December": 0,
+      }
+   },
+   {
       "year": "2022",
       "vacations": {
          "January": 20,
